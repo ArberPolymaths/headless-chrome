c84c955790ca18830af8c89e846df1e2e064a922
ArberPolymaths <arber.vrajolli@polymaths.co>
2022-08-05 14:44:11 +0200

    first commit

--- app/browser.js
+++ app/browser.js
@@ -2,12 +2,12 @@ const puppeteer = require ('puppeteer');
 
 (async () => {
     const browser = await puppeteer.launch({
-      headless: true,
+      headless: true, // deafault osht true ,po e ceki true per me qen ma e kjart
       executablePath: '/usr/bin/chromium-browser',
           args: [
             "--no-sandbox",
           ] 
     });
     const page = await browser.newPage();
-    // await browser.close();
+    // await browser.close();  // nuk po e mbylli browser ne ket rast per me mbet container running
 })();
